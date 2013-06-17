be6ac471cc9f3444620880c53c124440ff7e5f0c
Ross Spencer <all.along.the.watchtower2001+github@gmail.com>
2013-06-17 17:21:25 +1200

    [feature] Additional time stamp and count for file names.

--- recurse-dir.bat
+++ recurse-dir.bat
@@ -6,11 +6,23 @@ REM a single command on the files it finds
 set SCANDIR="C:\Working\nlnz-test-files\"
 set OUTDIR="C:\Working\tika-out\"
 
-FOR /R %SCANDIR% %%F IN (*.*) DO (
-  setlocal enabledelayedexpansion
-	set FILENAME="%OUTDIR%%%~nF.tika"
-	echo Path: %%F > !!FILENAME!!
-	java -jar tika-app-1.3.jar -m %%F >> !!FILENAME!!
-	echo Result output to !!FILENAME!!
-	endlocal
-)
+REM Generate a unique string to attach to file
+
+setlocal enabledelayedexpansion
+
+	REM Count to append to timestamp
+	SET /A COUNT=0
+	
+	REM Generate a timestamp #setlocal not working for !UNIQUE!
+	FOR /f "tokens=2-8 delims=/:. " %%A IN ("%date%:%time: =0%") DO SET UNIQUE="%%C%%A%%B%%D%%E%%G%"
+
+	FOR /R %SCANDIR% %%F IN (*.*) DO (	
+		set /A COUNT+=1
+		set FILENAME="%OUTDIR%!UNIQUE!-!COUNT!-%%~nF.tika"
+		echo !!UNIQUE!!
+		echo Path: %%F > !!FILENAME!!
+		java -jar tika-app-1.3.jar -m %%F >> !!FILENAME!!
+		echo Result output to !!FILENAME!!
+	)
+	
+endlocal
